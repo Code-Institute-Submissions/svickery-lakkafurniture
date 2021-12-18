f154f880e05c1b42121f64e24cfa8e082ce14ed7
svickery <s.vickery@hotmail.co.uk>
2021-12-18 22:44:05 +0000

    Add post form added and functional

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import blog, BlogDetail
+from .views import Blog, BlogDetail, AddPost
 
 urlpatterns = [
     # path('', views.blog, name='blog'),
-    path('', blog.as_view(), name="blog"),
+    path('', Blog.as_view(), name="blog"),
     path('article/<int:pk>', BlogDetail.as_view(), name='blog-article'),
+    path('add_post/', AddPost.as_view(), name="add_post"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from .forms import PostForm
 
 # Create your views here.
 # def blog(request):
 #     return render(request, 'blog.html', {})
 
-class blog(ListView):
+class Blog(ListView):
     model = Post
     template_name = 'blog.html'
 
@@ -14,3 +15,10 @@ class blog(ListView):
 class BlogDetail(DetailView):
     model = Post
     template_name = 'blog_details.html'
+
+
+class AddPost(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+    # fields = '__all__'
