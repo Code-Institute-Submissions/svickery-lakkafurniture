44f27709b34bef8bbf88800a3c1576e1f34f76a8
svickery <s.vickery@hotmail.co.uk>
2021-12-19 04:05:35 +0000

    Some Review logic added. Will allow users to review products on site

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ from ckeditor.fields import RichTextField
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    header_image = models.ImageField(null=True, blank=True, upload_to="images/")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = RichTextField(blank=True, null=True)
     # body = models.TextField()
