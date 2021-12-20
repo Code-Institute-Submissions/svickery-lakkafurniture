ce87fda061ef834c77ebf8406df89472c98cad46
svickery <s.vickery@hotmail.co.uk>
2021-12-20 23:47:16 +0000

    Default email changed in settings.py

--- lakka_furniture/settings.py
+++ lakka_furniture/settings.py
@@ -189,7 +189,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'lakkafurniture@example.com'
+    DEFAULT_FROM_EMAIL = 'lakkafurniture@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
