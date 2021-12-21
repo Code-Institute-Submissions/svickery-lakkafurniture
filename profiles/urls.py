6d83769b45a4a76a9f7db954e144f612108b61fa
svickery <s.vickery@hotmail.co.uk>
2021-12-21 02:28:23 +0000

    All python files checked for pep8 and comments added where needed

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    """Frontend Path ways"""
     path('', views.profile, name='profile'),
-    path('order_history/<order_number>', views.order_history, name='order_history'),
+    path('order_history/<order_number>',
+         views.order_history, name='order_history'),
 ]
